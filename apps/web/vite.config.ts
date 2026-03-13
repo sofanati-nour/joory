c19c7008bf30413272a2c5960113b7026d9e28a3
Nour Sofanati <nour@simplebackups.com>
2026-03-13 03:24:03 +0300

    s

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -9,7 +9,22 @@ export default defineConfig({
 	optimizeDeps: { exclude: ['@app/shared', 'zod'] },
 	build: {
 		rollupOptions: {
-			external: ['shiki', 'shiki/core', 'shiki/engine/javascript', 'shiki/langs', 'shiki/themes/nord']
+			external: [
+				'shiki',
+				'shiki/core',
+				'shiki/engine/javascript',
+				'shiki/langs',
+				'shiki/themes/nord',
+				'@shikijs/langs/javascript',
+				'@shikijs/langs/typescript',
+				'@shikijs/langs/html',
+				'@shikijs/langs/css',
+				'@shikijs/langs/python',
+				'@shikijs/langs/bash',
+				'@shikijs/langs/json',
+				'@shikijs/langs/svelte',
+				'@shikijs/langs/php'
+			]
 		}
 	},
 	envDir: resolve(__dirname, '../..')
